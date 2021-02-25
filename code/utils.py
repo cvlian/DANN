d179d51810eebefe2c7b8cc3028d9a91ca6862f8
Cvilian <Cvilian003@gmail.com>
2021-02-25 21:26:32 +0900

    minor update

--- code/utils.py
+++ code/utils.py
@@ -98,7 +98,7 @@ def visualize_tsne(x, y):
     """
     n = x.shape[0]
 
-    model = TSNE(learning_rate=500, n_components=2, random_state=0, n_iter=250, init='pca')
+    model = TSNE(learning_rate=100, n_components=2, random_state=0, n_iter=1000)
     transformed = model.fit_transform(x)
     
     plt.rc('font',family='DejaVu Sans', size=14)
@@ -110,65 +110,3 @@ def visualize_tsne(x, y):
     plt.legend()
     plt.show()
 
-def visualize_sole_acc(res, maxiter=10, prev_res=None):
-    """
-    Plot accuracy (one model)
-    """
-    plt.rc('font',family='DejaVu Sans', size=16)
-    fig=plt.figure(figsize=(5, 4.5))
-    ax=fig.add_axes([0,0,1,1])
-    
-    for task, reports in res.items() :
-        if prev_res != None and task in prev_res :
-            plt.plot(list(range(0, maxiter+1)), [prev_res[task][-1]]+reports[:maxiter], label=task, markersize=8, linewidth=3, clip_on=False)
-        else :
-            plt.plot(list(range(0, maxiter+1)), [0.0]+reports[:maxiter], label=task, markersize=8, linewidth=3, clip_on=False)
-
-    ax.set_xlim([0, maxiter])
-    ax.set_ylim([0.0, 1.0])
-    ax.set_xticks(list(range(0, maxiter+1, maxiter//5)))
-    ax.set_yticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0])
-    ax.set_xticks(range(0, maxiter+1, maxiter//10), minor=True)
-    ax.set_yticks([i/10 for i in range(0, 11)], minor=True)
-    ax.set_xlabel("Epochs", fontsize=20)
-    ax.set_ylabel("Accuracy", fontsize=20)
-    ax.legend(markerscale=1, fontsize=16, loc='lower right')
-    ax.grid(which='both', color='#BDBDBD', linestyle='--', linewidth=1)
-    plt.rc('font',family='DejaVu Sans', size=16)
-
-    plt.show()
-
-def visualize_multi_acc(models, maxiter=10, tasks=3):
-    """
-    Plot accuracy (multiple models)
-    """
-    fig, axs = plt.subplots(tasks, tasks, figsize=(12, 11.5))
-    plt.rc('font',family='DejaVu Sans', size=14)
-    
-    for i in range(tasks) :
-        for j in range(0, i):
-            axs[i, j].axis('off')
-        for j in range(i, tasks):
-            for task_name, reports in models[i].res[j-i].items() :
-                if j > i and task_name in models[i].res[j-i-1] :
-                    axs[i, j].plot(list(range(0, maxiter+1)),
-                                   [models[i].res[j-i-1][task_name][-1]]+reports[:maxiter],
-                                   label=task_name, linewidth=3, clip_on=False)
-                else :
-                    axs[i, j].plot(list(range(0, maxiter+1)), 
-                                   [0.0]+reports[:maxiter], 
-                                   label=task_name, linewidth=3, clip_on=False)
-                axs[i, j].set_xlim([0, maxiter])
-                axs[i, j].set_ylim([0.0, 1.0])
-                axs[i, j].set_xticklabels(list(range(0, maxiter+1, maxiter//5)), fontsize=14)
-                axs[i, j].set_yticklabels([0.0, 0.2, 0.4, 0.6, 0.8, 1.0], fontsize=14)
-                axs[i, j].set_xticks(range(0, maxiter+1, maxiter//10), minor=True)
-                axs[i, j].set_yticks([i/10 for i in range(0, 11)], minor=True)
-                if i == j :
-                    axs[i, j].set_xlabel("Epochs", fontsize=18)
-                    axs[i, j].set_ylabel("Accuracy", fontsize=18)
-                axs[i, j].legend(markerscale=1, fontsize=14, loc='lower right')
-                axs[i, j].grid(which='both', color='#BDBDBD', linestyle='--', linewidth=1)
-    
-    fig.tight_layout()
-    plt.show()
